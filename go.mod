625e46cc02fb6da010c91db40e41bdc25a8c541d
steveyiyo <jackcooku@steveyi.net>
2021-04-12 04:17:13 +0800

    Install

---
+++ go.mod
@@ -0,0 +1,11 @@
+module steveyi-bot
+
+go 1.16
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/likexian/whois v1.9.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"github.com/likexian/whois"
+)
+
+func main() {
+	godotenv.Load()
+
+	TGBotToken := os.Getenv("Telegram_Bot_Token")
+
+	bot, err := tgbotapi.NewBotAPI(TGBotToken)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Success!")
+	}
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		log.Println(update.Message.Chat.ID, update.Message.Text)
+		switch update.Message.Command() {
+		case "whois":
+			content := strings.ReplaceAll(update.Message.Text, "/whois", "")
+			result, err := whois.Whois(content)
+			if err != nil {
+				fmt.Println(result)
+			}
+			msg.Text = "`" + result + "`"
+		case "mtr":
+			// https://github.com/steveyiyo/LookingGlassBot
+		case "ping":
+			// https://github.com/steveyiyo/LookingGlassBot
+		case "traceroute":
+			// https://github.com/steveyiyo/LookingGlassBot
+		default:
+			msg.Text = "唉呀！真糟糕。我看不懂，你在說什麼"
+		}
+		msg.ParseMode = "markdown"
+
+		if _, err := bot.Send(msg); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
